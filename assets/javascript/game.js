5094dad810a514c05a6a206e72da825c1a37d241
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-05-28 15:03:24 -0700

    additional regular player vs. player functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,32 +5,42 @@ var classGame;
 var nbaGame;
 var you;
 var opponent;
+var numOpponents = 3;
+var gameOver = false;
 
 var stephC = {
     hP: 200,
+    currenthP: 200,
     counterAttack: 25,
-    attack: 20, 
+    attack: 20,
+    currentAttack: 20,
     name: "Steph"
 }
 
 var kawhiL = {
     hP: 175,
+    currenthP: 175,
     counterAttack: 30,
-    attack: 15, 
+    attack: 15,
+    currentAttack: 15,
     name: "Kawhi"
 }
 
 var lebronJ = {
     hP: 150,
+    currenthP: 150,
     counterAttack: 35,
-    attack: 10, 
+    attack: 10,
+    currentAttack: 10,
     name: "LeBron"
 }
 
 var kevinD = {
     hP: 125,
+    currenthP: 125,
     counterAttack: 40,
-    attack: 5, 
+    attack: 5,
+    currentAttack: 5,
     name: "KD"
 }
 
@@ -43,9 +53,15 @@ function resetGame() {
 }
 
 function setClassMode() {
-    for(var i = 0; i < 4; i++) {
+    for (var i = 0; i < 4; i++) {
         $("#" + players[i].name).text(players[i].name + ": " + players[i].hP);
     }
+    $("#att1").text("Attack Power:");
+    $("#att2").text("Counter Attack Power: ");
+    $("#att3").text("Attack Power:");
+    $("#att4").text("Counter Attack Power: ");
+    $("#gamePlay").text('Attack');
+
 }
 
 resetGame();
@@ -72,61 +88,96 @@ console.log(this);
 
 
 $(".pic").on("click", function () {
-    if (choosePlayer) {
-        startGame = false;
-        you = players[this.getAttribute("data-index")];
-        $("#you").attr("src", this.id);
-        $("#you-text").css('color', this.getAttribute("data-color"));
-        $("#you-text").text(this.getAttribute("data-name"));
-        if(classGame) {
-            $("#you-text").text(this.getAttribute("data-name") + ": " + you.hP); 
+    if (!gameOver) {
+        if (choosePlayer) {
+            startGame = false;
+            you = players[this.getAttribute("data-index")];
+            $("#you").attr("src", this.id);
+            $("#you-text").css('color', this.getAttribute("data-color"));
+            $("#you-text").text(this.getAttribute("data-name"));
+            if (classGame) {
+                $("#you-text").text(this.getAttribute("data-name") + ": " + you.hP);
+                $("#att1").text("Attack Power: " + you.attack);
+                $("#att2").text("Counter Attack Power: " + you.counterAttack);
+            }
+            choosePlayer = false;
+            chooseOpponent = true;
+            // how to get this to take precedence over for loop
+            $("#choiceOfPlayer").hide();
+            $("#choiceOfOpponent").show();
+            console.log(fadeInAndOut);
+            console.log(this);
+            //Why can this not be called from here!!!
+            // $("#chooseOpponent").fadeOut();
+            // fadeInAndOut("choiceOfOpponent");
+            // you = players[this.getAttribute("data-index")];
         }
-        choosePlayer = false;
-        chooseOpponent = true;
-        // how to get this to take precedence over for loop
-        $("#choiceOfPlayer").hide();
-        $("#choiceOfOpponent").show();
-        console.log(fadeInAndOut);
-        console.log(this);
-        //Why can this not be called from here!!!
-        // $("#chooseOpponent").fadeOut();
-        // fadeInAndOut("choiceOfOpponent");
-        // you = players[this.getAttribute("data-index")];
-    }
-    else if (chooseOpponent) {
-        opponent = players[this.getAttribute("data-index")];
-        $("#opponent").attr("src", this.id);
-        $("#opponent-text").css('color', this.getAttribute("data-color"));
-        $("#opponent-text").text(this.getAttribute("data-name"));
-        if(classGame) {
-            $("#opponent-text").text(this.getAttribute("data-name") + ": " + opponent.hP);
+        else if (chooseOpponent) {
+            opponent = players[this.getAttribute("data-index")];
+            $("#opponent").attr("src", this.id);
+            $("#opponent-text").css('color', this.getAttribute("data-color"));
+            $("#opponent-text").text(this.getAttribute("data-name"));
+            if (classGame) {
+                $("#opponent-text").text(this.getAttribute("data-name") + ": " + opponent.hP);
+                $("#att3").text("Attack Power: " + opponent.attack);
+                $("#att4").text("Counter Attack Power: " + ": " + opponent.counterAttack);
+            }
+            chooseOpponent = false;
+            $("#choiceOfOpponent").hide();
         }
-        chooseOpponent = false;
-        $("#choiceOfOpponent").hide();
     }
 })
 
-// $("#.gamePlay").on("click", function () {
-//     if(you!== null && opponent !== null) {
-//         if(classGame) {
+$("#gamePlay").on("click", function () {
+    if (!gameOver) {
+        if (you !== null && opponent !== null) {
+            if (classGame) {
+                you.currentAttack += you.attack;
+                opponent.currenthP -= you.currentAttack;
+                you.currenthP -= opponent.counterAttack;
+                $("#att1").text("Attack Power: " + you.currentAttack);
+                $("#you-text").text(you.name + ": " + you.currenthP);
+                $("#opponent-text").text(opponent.name + ": " + opponent.currenthP);
+                if (you.currenthP <= 0) {
+                    gameOver = true;
+                    console.log("You Lose");
+                }
+                else if (opponent.currenthP <= 0) {
+                    opponent = null;
+                    $("#opponent").attr("src", "");
+                    $("#opponent-text").text(this.getAttribute(""));
+                    numOpponents--;
+                    if (numOpponents !== 0) {
+                        chooseOpponent = true;
+                        $("#choiceOfOpponent").show();
+                    }
+                    else {
+                        gameOver = true;
+                        console.log("You win");
+                    }
+                }
+            }
+            else if (nbaGame) {
 
-//         }
-//         else if(nbaGame) {
-
-//         }
-//     }
-// })
-
-$("#mode").on("click", function () {
-    if(startGame) {
-        if(this.getAttribute("data-value") === "nba") {
-            classGame = false;
+            }
         }
-        else {
-            nbaGame = false;
-            classGame = true;
-            setClassMode();
+    }
+})
+
+$(".mode").on("click", function () {
+    if (!gameOver) {
+        if (startGame) {
+            console.log(this.getAttribute("data-value"));
+            if (this.getAttribute("data-value") === "nba") {
+                console.log("We should have gotten here");
+                classGame = false;
+            }
+            else {
+                nbaGame = false;
+                classGame = true;
+                setClassMode();
+            }
+            startGame = false;
         }
-        startGame = false;
     }
 })
