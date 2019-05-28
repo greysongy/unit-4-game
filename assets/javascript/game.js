fec64e5ffa44397f9bb849ceac5448d23935ae7e
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-05-27 22:41:38 -0700

    some additional alert functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,32 @@
+var choosePlayer;
+
+function resetGame() {
+    choosePlayer = true;
+}
+
+resetGame();
+
+// for (var i = 0; i < 100; i++) {
+//     console.log(choosePlayer);
+//     if (choosePlayer === true) {
+//         $("#choosePlayer").fadeIn("slow");
+//         $("#choosePlayer").fadeOut("slow");
+//     }
+// }
+
+for(var i = 0; i < 5; i++) {
+    $("#choosePlayer").fadeOut();
+    $("#choosePlayer").fadeIn();
+}
+
 $(".pic").on("click", function () {
-    var you = this.id;
-    $("#you").attr("src", you);
-    $("#you-text").css('color', this.getAttribute("data-color"));
-    $("#you-text").text(this.getAttribute("data-name"));
+    if (choosePlayer) {
+        var you = this.id;
+        $("#you").attr("src", you);
+        $("#you-text").css('color', this.getAttribute("data-color"));
+        $("#you-text").text(this.getAttribute("data-name"));
+        choosePlayer = false;
+        // how to get this to take precedence over for loop
+        $("#choosePlayer").hide()
+    }
 })
