f116d1314f1fcb8ba54839207b4f1326d543f6e1
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-05-28 17:25:59 -0700

    Second mode with more realistic NBA functionality complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,11 @@ var stephC = {
     attack: 9,
     currentAttack: 9,
     name: "Steph",
-    validOpponent: true
+    validOpponent: true,
+    points: 0,
+    threePer: .437,
+    twoPer: .525,
+    color: "#ffd700"
 }
 
 var kawhiL = {
@@ -25,7 +29,11 @@ var kawhiL = {
     attack: 6,
     currentAttack: 6,
     name: "Kawhi",
-    validOpponent: true
+    validOpponent: true,
+    points: 0,
+    threePer: .371,
+    twoPer: .542,
+    color: "#ff0000"
 }
 
 var lebronJ = {
@@ -35,7 +43,11 @@ var lebronJ = {
     attack: 8,
     currentAttack: 8,
     name: "LeBron",
-    validOpponent: true
+    validOpponent: true,
+    points: 0,
+    threePer: .339,
+    twoPer: .582,
+    color: "#800080"
 }
 
 var kevinD = {
@@ -45,7 +57,11 @@ var kevinD = {
     attack: 7,
     currentAttack: 7,
     name: "KD",
-    validOpponent: true
+    validOpponent: true,
+    points: 0,
+    threePer: .353,
+    twoPer: .587,
+    color: "#ffd700"
 }
 
 var players = [stephC, kawhiL, lebronJ, kevinD]
@@ -65,6 +81,8 @@ function setClassMode() {
     $("#att3").text("Attack Power:");
     $("#att4").text("Counter Attack Power: ");
     $("#gamePlay").text('Attack');
+    $("#youScore").hide();
+    $("#oppScore").hide();
 
 }
 
@@ -104,6 +122,13 @@ $(".pic").on("click", function () {
                 $("#att1").text("Attack Power: " + you.attack);
                 $("#att2").text("Counter Attack Power: " + you.counterAttack);
             }
+            else {
+                console.log("color: " + you.color);
+                $("#youScore").css("color", you.color);
+                $("#youScore").text(0);
+                $("#att1").text("2P %: " + you.twoPer);
+                $("#att2").text("3P %: " + you.threePer);
+            }
             choosePlayer = false;
             chooseOpponent = true;
             you.validOpponent = false;
@@ -129,6 +154,12 @@ $(".pic").on("click", function () {
                     $("#opponent-text").text(this.getAttribute("data-name") + ": " + opponent.hP);
                     $("#att3").text("Attack Power: " + opponent.attack);
                     $("#att4").text("Counter Attack Power: " + ": " + opponent.counterAttack);
+                }
+                else {
+                    $("#oppScore").css("color", opponent.color);
+                    $("#oppScore").text(0);
+                    $("#att3").text("2P %: " + opponent.twoPer);
+                    $("#att4").text("3P %: " + opponent.threePer);
                 }
                 chooseOpponent = false;
                 this.validOpponent = false;
@@ -176,6 +207,79 @@ $("#gamePlay").on("click", function () {
                 }
             }
             else if (nbaGame) {
+                if (Math.random() < .5) {
+                    if (Math.random() < .5) {
+                        if (Math.random() < you.twoPer) {
+                            you.points += 2;
+                        }
+                        if (Math.random() < opponent.twoPer) {
+                            opponent.points += 2;
+                        }
+                    }
+                    else {
+                        if (Math.random() < you.twoPer) {
+                            you.points += 2;
+                        }
+                        if (Math.random() < opponent.threePer) {
+                            opponent.points += 3;
+                        }
+                    }
+                }
+                else {
+                    if (Math.random() < .5) {
+                        if (Math.random() < you.threePer) {
+                            you.points += 3;
+                        }
+                        if (Math.random() < opponent.twoPer) {
+                            opponent.points += 2;
+                        }
+                    }
+                    else {
+                        if (Math.random() < you.threePer) {
+                            you.points += 3;
+                        }
+                        if (Math.random() < opponent.threePer) {
+                            opponent.points += 3;
+                        }
+                    }
+                }
+                $("#youScore").text(you.points);
+                $("#oppScore").text(opponent.points);
+                if (you.points >= 11 || opponent.points >= 11) {
+                    if (you.points > opponent.points) {
+                        $("#oppScore").text(0);
+                        $("#opponent").attr("src", "");
+                        $("#opponent-text").text(this.getAttribute(""));
+                        $("." + opponent.name + "Image").hide();
+                        $("." + opponent.name + "Label").text("");
+                        $("#att3").text("2P %: ");
+                        $("#att4").text("3P %: ");
+                        opponent = null;
+                        numOpponents--;
+                        if (numOpponents !== 0) {
+                            chooseOpponent = true;
+                            $("#choiceOfOpponent").show();
+                        }
+                        else {
+                            gameOver = true;
+                            $("#leftText").text("You");
+                            $("#rightText").text("Win");
+                        }
+                    }
+                    else if (you.points < opponent.points) {
+                        gameOver = true;
+                        $("#leftText").text("You");
+                        $("#rightText").text("Lose");
+                    }
+
+                    else {
+                        gameOver = true;
+                        $("#leftText").text("You");
+                        $("#rightText").text("Tie");
+                    }
+                    you.points = 0;
+                    $("#youScore").text(you.points);
+                }
 
             }
         }
